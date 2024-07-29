11f032f6df96a88bdcbd08d60ded5a41f494b9c4
apri017 <waodeapri2@gmail.com>
2024-07-29 13:17:18 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll('.section');
+    const navLi = document.querySelectorAll('.sidebar nav ul li a');
+
+    window.addEventListener('scroll', () => {
+        let current = '';
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
+                current = section.getAttribute('id');
+            }
+        });
+
+        navLi.forEach(li => {
+            li.classList.remove('active');
+            if (li.getAttribute('href') === `#${current}`) {
+                li.classList.add('active');
+            }
+        });
+    });
+
+    navLi.forEach(li => {
+        li.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
